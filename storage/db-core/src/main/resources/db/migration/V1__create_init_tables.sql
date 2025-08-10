1565bbf236ef3362b0e85a0ced694ce1ebf76615
re4rk <re4rks@gmail.com>
2025-08-10 18:30:12 +0900

    ARK-003 SQL 코맨트 추가

--- storage/db-core/src/main/resources/db/migration/V1__create_init_tables.sql
+++ storage/db-core/src/main/resources/db/migration/V1__create_init_tables.sql
@@ -12,11 +12,11 @@ CREATE TABLE IF NOT EXISTS `example_entity` (
 CREATE TABLE IF NOT EXISTS files (
     `id`                    BIGINT          NOT NULL AUTO_INCREMENT,
 
-    `original_name`         VARCHAR(255)    NOT NULL,
-    `s3_key`                VARCHAR(255)    NOT NULL,
-    `size`                  BIGINT          NOT NULL,
-    `mime_type`             VARCHAR(255)    NOT NULL,
-    `status`                VARCHAR(32)     NOT NULL,
+    `original_name`         VARCHAR(255)    NOT NULL COMMENT '원본 파일 이름',
+    `s3_key`                VARCHAR(255)    NOT NULL COMMENT 'S3에 저장된 파일의 키',
+    `size`                  BIGINT          NOT NULL COMMENT '파일 크기 (바이트 단위)',
+    `mime_type`             VARCHAR(255)    NOT NULL COMMENT '파일의 MIME 타입',
+    `status`                VARCHAR(32)     NOT NULL COMMENT '파일 상태 (예: UPLOADED, PROCESSING, COMPLETED, FAILED)',
 
     `created_at`            TIMESTAMP(6)    NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
     `updated_at`            TIMESTAMP(6)    NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
