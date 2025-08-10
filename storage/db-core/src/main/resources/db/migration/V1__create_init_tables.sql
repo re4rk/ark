5581312a09f20322033f09c5f81eea3c4d235e4c
re4rk <re4rks@gmail.com>
2025-08-11 01:42:06 +0900

    ARK-003 Flyway 린트적용

--- storage/db-core/src/main/resources/db/migration/V1__create_init_tables.sql
+++ storage/db-core/src/main/resources/db/migration/V1__create_init_tables.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS files (
     `id`                    BIGINT          NOT NULL AUTO_INCREMENT,
 
     `original_name`         VARCHAR(255)    NOT NULL COMMENT '원본 파일 이름',
-    `file_key`                   VARCHAR(255)    NOT NULL COMMENT '저장된 파일의 키',
+    `file_key`              VARCHAR(255)    NOT NULL COMMENT '저장된 파일의 키',
     `size`                  BIGINT          NOT NULL COMMENT '파일 크기 (바이트 단위)',
     `mime_type`             VARCHAR(255)    NOT NULL COMMENT '파일의 MIME 타입',
     `status`                VARCHAR(32)     NOT NULL COMMENT '파일 상태 (예: UPLOADED, PROCESSING, COMPLETED, FAILED)',
