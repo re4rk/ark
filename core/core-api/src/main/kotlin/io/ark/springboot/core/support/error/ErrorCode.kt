0e0ac24222d6df61ded822d37b6c34a78b403545
re4rk <re4rks@gmail.com>
2025-08-11 00:45:23 +0900

    ARK-003 에러 코드 수정

--- core/core-api/src/main/kotlin/io/ark/springboot/core/support/error/ErrorCode.kt
+++ core/core-api/src/main/kotlin/io/ark/springboot/core/support/error/ErrorCode.kt
@@ -1,10 +1,12 @@
 package io.ark.springboot.core.support.error
 
 enum class ErrorCode {
-    E100,
-    E400,
-    E401,
-    E403,
-    E404,
+    FILE_UNSUPPORTED_TYPE,
+    FILE_SIZE_EXCEEDED,
+    FILE_MISSING,
+    FILE_NOT_FOUND,
+    FILE_ACCESS_DENIED,
+    FILE_UPLOAD_ERROR,
+    FILE_TOO_MANY_UPLOADS,
     E500,
 }
--- core/core-api/src/main/kotlin/io/ark/springboot/core/support/error/ErrorType.kt
+++ core/core-api/src/main/kotlin/io/ark/springboot/core/support/error/ErrorType.kt
@@ -7,11 +7,11 @@ enum class ErrorType(val status: HttpStatus, val code: ErrorCode, val message: S
     DEFAULT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.E500, "An unexpected error has occurred.", LogLevel.ERROR),
 
     // 파일 관련 오류
-    FILE_UNSUPPORTED_TYPE(HttpStatus.BAD_REQUEST, ErrorCode.E400, "지원하지 않는 파일 형식입니다.", LogLevel.WARN),
-    FILE_SIZE_EXCEEDED(HttpStatus.BAD_REQUEST, ErrorCode.E400, "파일 크기가 제한을 초과했습니다.", LogLevel.WARN),
-    FILE_MISSING(HttpStatus.BAD_REQUEST, ErrorCode.E400, "업로드할 파일이 없습니다.", LogLevel.WARN),
-    FILE_NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.E404, "파일을 찾을 수 없습니다.", LogLevel.WARN),
-    FILE_ACCESS_DENIED(HttpStatus.FORBIDDEN, ErrorCode.E403, "파일에 접근할 권한이 없습니다.", LogLevel.WARN),
-    FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.E500, "파일 업로드 중 오류가 발생했습니다.", LogLevel.ERROR),
-    FILE_TOO_MANY_UPLOADS(HttpStatus.BAD_REQUEST, ErrorCode.E400, "동시 업로드 파일 수가 제한을 초과했습니다.", LogLevel.WARN),
+    FILE_UNSUPPORTED_TYPE(HttpStatus.BAD_REQUEST, ErrorCode.FILE_UNSUPPORTED_TYPE, "지원하지 않는 파일 형식입니다.", LogLevel.WARN),
+    FILE_SIZE_EXCEEDED(HttpStatus.BAD_REQUEST, ErrorCode.FILE_SIZE_EXCEEDED, "파일 크기가 제한을 초과했습니다.", LogLevel.WARN),
+    FILE_MISSING(HttpStatus.BAD_REQUEST, ErrorCode.FILE_MISSING, "업로드할 파일이 없습니다.", LogLevel.WARN),
+    FILE_NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.FILE_NOT_FOUND, "파일을 찾을 수 없습니다.", LogLevel.WARN),
+    FILE_ACCESS_DENIED(HttpStatus.FORBIDDEN, ErrorCode.FILE_ACCESS_DENIED, "파일에 접근할 권한이 없습니다.", LogLevel.WARN),
+    FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.FILE_UPLOAD_ERROR, "파일 업로드 중 오류가 발생했습니다.", LogLevel.ERROR),
+    FILE_TOO_MANY_UPLOADS(HttpStatus.BAD_REQUEST, ErrorCode.FILE_TOO_MANY_UPLOADS, "동시 업로드 파일 수가 제한을 초과했습니다.", LogLevel.WARN),
 }
