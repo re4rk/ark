dfe986e3554346df56bd5f06719d9f4c065ce161
re4rk <re4rks@gmail.com>
2025-09-09 22:05:56 +0900

    ARK-005 로직 축소

--- core/core-api/src/main/kotlin/io/ark/springboot/core/domain/user/UserService.kt
+++ core/core-api/src/main/kotlin/io/ark/springboot/core/domain/user/UserService.kt
@@ -15,27 +15,17 @@ class UserService(
 ) {
     @Transactional
     fun signUp(userData: UserData): User {
-        // 이메일 중복 확인
         if (userStorage.existsByEmail(userData.email)) {
             throw CoreException(ErrorType.USER_EMAIL_ALREADY_EXISTS)
         }
 
-        // 사용자명 중복 확인
         if (userStorage.existsByUsername(userData.username)) {
             throw CoreException(ErrorType.USER_USERNAME_ALREADY_EXISTS)
         }
 
-        // 비밀번호 암호화
         val encodedPassword = passwordEncoder.encode(userData.password)
-
-        // 사용자 데이터 생성
         val formattedUserData = userData.copy(password = encodedPassword)
-
-        // 사용자 저장
-        val savedUser = userStorage.save(formattedUserData)
-
-        // 응답 DTO 생성
-        return savedUser
+        return userStorage.save(formattedUserData)
     }
 
     fun findByEmail(email: String): User {
